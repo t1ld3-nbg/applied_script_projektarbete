2d1b666968a5e827ce5429b21edb9763c4f58d96
Tilde <norberg.tilde@gmail.com>
2026-01-08 16:31:38 +0100

    Första commit av projektarbetet för Applied Script.

---
+++ README.md
@@ -0,0 +1,1 @@
+Projektuppgift för kursen Applied Script, januari 2026. Ett bash script för att logga varje gång en användare loggar in i terminalen.
---
+++ login_checker_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Login Check logs all logins with date, time and user.
+
+#Saves the log output to a log file.
+LOG_FILE= "/var/log/login_checker.log"
+
+
+
