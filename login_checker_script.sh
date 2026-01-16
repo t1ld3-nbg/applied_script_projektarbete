fe8a75d5edbf59ddd0d84f31c9f656cce11268cc
Tilde <norberg.tilde@gmail.com>
2026-01-16 12:43:47 +0100

    Adding script to trigger background logging with PAM.

--- login_checker_script.sh
+++ login_checker_script.sh
@@ -1,47 +1,39 @@
 #!/bin/bash
 # Login Checker logs all logins with date, time, user, and IP-address.
 
-<<<<<<< HEAD
-# Check for Linux OS otherwise exit script
+# Check for Linux OS otherwise exit script.
 if [[ "$(uname)" != "Linux" ]]; then
     echo "Error: This script only runs on Linux." >&2
     exit 1
 fi
 
+
 # Saves the log output to a log file.
 LOG_FILE="/var/log/login_checker.log"
 
-# Variable for IP-address.
-IP_ADDRESS=$(ip a show scope global | awk '/inet / {print $2}' | \
-cut -d/ -f1)
+# Using PAM to trigger script att login.
 
-# Logs time and date as well as which user, 
-#	and their terminal and IP address.
-=======
-# Saves the log output to a log file.
-#LOG_FILE="/var/log/login_checker.log"
+# PAM username
+USERNAME="${PAM_USER:-$USER}
 
-# Checks for file and file path, creates them if they don't already exist.
-#[[ -e $LOG_FILE ]] || touch $LOG_FILE 
+# PAM terminal
+TERMINAL="${PAM_TTY:-$(tty))"
 
- Logs time and date as well which user logged in using what terminal.
-#>>>>>>> 3bc5294 (Checks for file + path for storing output of the script. Creates file + path if the don't already exist.)
+# PAM IP: Remote host IP.
+if [[ -n "$PAM_RHOST" && "$PAM_RHOST" != "(:0)" ]]; then
+	IP_ADDRESS="$PAM_RHOST"
+else
+	#Local login, check non-PAM  IP.
+	IP_ADDRESS=$(ip a show scope global | awk '/inet / {print $2}' | cut -d/ -f1
+fi
+
+
+# Log file output.
 {
-	echo "$(date '+%Y-%m-%d %H:%M:%S')  User $USER logged in from \
-$(tty) using IP address $IP_ADDRESS"
-} >> "$LOG_FILE"
-#<<<<<<< HEAD
-exit 0
-=======
-exit 0
+	echo "$(date '+%Y-%m-%d %H:%M:%S')   User $USERNAME logged in from $TERMINAL \
+using IP address $IP_ADDRESS"
+} >> $LOG_FILE"
 
 
 
 
-
-
-
-
-
-
->>>>>>> 3bc5294 (Checks for file + path for storing output of the script. Creates file + path if the don't already exist.)
